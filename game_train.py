ed623d4df1c24eb2b330147eea78438b9319e009
sharedcare <sharedcare@hotmail.com>
2018-07-18 21:33:00 +0800

    Add train process

---
+++ game_train.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+from __future__ import print_function
+from pong import PongGame
+
+
+def train(num_iteration):
+    pass
+
+if __name__ == '__main__':
+    pong_game = PongGame()
